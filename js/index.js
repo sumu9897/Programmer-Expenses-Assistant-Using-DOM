0ff14f7c7346599b03365914be89090b4bb6fdff
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-09-23 08:22:11 +0600

    Design Full Part Done

--- js/index.js
+++ js/index.js
@@ -92,3 +92,25 @@ historyTab.addEventListener('click', function(){
     document.getElementById('expense-form').classList.add('hidden');
     document.getElementById('history-section').classList.remove('hidden');
 })
+
+assistantTab.addEventListener('click',function(){
+
+    historyTab.classList.remove(
+        'text-white',
+        'bg-gradient-to-r',
+        'from-blue-500',
+        'to-purple-600'
+    );
+    historyTab.classList.add("text-gray-600");
+
+    assistantTab.classList.add(
+        'text-white',
+        'bg-gradient-to-r',
+        'from-blue-500',
+        'to-purple-600'
+    );
+    assistantTab.classList.remove("text-gray-600");
+
+    document.getElementById('expense-form').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+})
