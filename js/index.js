a9ec4ff387c9a64fc7a951de8dce3ed5df21c822
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-09-23 08:07:22 +0600

    History Button Function done

--- js/index.js
+++ js/index.js
@@ -25,6 +25,18 @@ calculateButton.addEventListener ("click", function () {
 
     const result = document.getElementById('results');
     result.classList.remove('hidden');
+
+    const historyItem = document.createElement("div");
+    historyItem.className = 'bg-white p-3 rounded-md border-l-2 border-indigo-500';
+
+    historyItem.innerHTML= `
+    <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+    <p class="text-xs text-gray-500">Income :$${income.toFixed(2)}</p>
+    <p class="text-xs text-gray-500">Expenses :$${totalExpenses.toFixed(2)}</p>
+    <p class="text-xs text-gray-500">Balance :$${balance.toFixed(2)}</p>
+    `;
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 });
 
 
@@ -53,4 +65,30 @@ calculateSavingButton.addEventListener('click', function () {
     remainingElement.innerText = remainingBalance.toFixed(2);
 
 
+})
+
+
+// History Tab Functionality
+const historyTab = document.getElementById('history-tab');
+const assistantTab = document.getElementById('assistant-tab');
+historyTab.addEventListener('click', function(){
+
+    historyTab.classList.add(
+        'text-white',
+        'bg-gradient-to-r',
+        'from-blue-500',
+        'to-purple-600'
+    );
+    historyTab.classList.remove("text-gray-600");
+
+    assistantTab.classList.remove(
+        'text-white',
+        'bg-gradient-to-r',
+        'from-blue-500',
+        'to-purple-600'
+    );
+    assistantTab.classList.add("text-gray-600");
+
+    document.getElementById('expense-form').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
 })
